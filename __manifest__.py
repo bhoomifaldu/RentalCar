3e468d811abec59a321efbfe12ff8ab8993648e3
Bhoomi <bkf@Falcon.merlintecsol.net>
2018-07-18 18:25:40 +0530

    vehical master add in rental car module

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Vehical Master',
+    'version': '1.0',
+    'category': 'Product',
+    'author' : 'Bhoomi Patel',
+    'sequence': 60,
+    'summary': 'Vehical Master',
+    'description': """Vehical Master""",
+    'depends': ['base'],
+    'data': [
+            'views/vehical_master.xml',
+            # 'security/ir.model.access.csv',
+	],
+
+    'installable': True,
+    'auto_install': False,
+    'active': True,
+}
---
+++ models/vehical_master.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+from odoo import api, fields, models, _
+
+class Vehical(models.Model):
+    _name = 'vehical.master'
+
+    name = fields.Char('Model No')
+    registration_no = fields.Char(string='Registration No')
+    color = fields.Char(string='Color')
+    value = fields.Float(string='Value',help='Vehical Price')
+    purchase_date = fields.Date(string='Purchase Date')
+    mileage = fields.Float(string='Mileage', help='Vehical Mileage')
+    meter_reading = fields.Float(string='Meter Reading', help='Last Meter Reading')
+    capacity = fields.Float(string='Passenger Capacity')
+    type = fields.Selection([('ac','AC'),('non_ac','NON AC')],'Type',default='ac')
+    fuel = fields.Float(string='Fuel Reading', help='Current Fuel Reading')
+    state = fields.Selection([
+        ('maintenance', 'Maintenance'),
+        ('active', 'Active'),
+		('inactive', 'Inactive')
+        ], string='Status', readonly=True, default='inactive')
