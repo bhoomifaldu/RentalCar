9dd519f07c14c39d9b0b53ad149bf58e737ae9c3
Bhoomi <bkf@Falcon.merlintecsol.net>
2018-08-09 19:07:46 +0530

    remove field

--- models/vehical_master.py
+++ models/vehical_master.py
@@ -6,7 +6,6 @@ class Vehical(models.Model):
     _name = 'vehical.master'
 
     name = fields.Char('Model No')
-    year = fields.Date(default=datetime.now().strftime("%m-%Y"))
     registration_no = fields.Char(string='Registration No')
     color = fields.Char(string='Color')
     value = fields.Float(string='Value',help='Vehical Price')
